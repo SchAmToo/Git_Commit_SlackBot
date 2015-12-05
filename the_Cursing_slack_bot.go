cf7eea842475ccd03cef01c413291e9cff01432b
Schamtoo <thekevinpearson@gmail.com>
2015-12-05 17:50:56 -0500

    new file:   .gitignore 	modified:   the_Cursing_slack_bot.go

--- the_Cursing_slack_bot.go
+++ the_Cursing_slack_bot.go
@@ -13,6 +13,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt" 
 	"net/http"
 	"bytes"
@@ -22,7 +23,29 @@ func main() {
 	//goroutine grabbing rss feeds from yaml(?) of each username
 	//goroutine of pushing that to slackbot
 	//go github_rss_feed(username)
-	 slack_bot_post("Hello, Tech-decks")
+	//slack_bot_post("Hello, Tech-decks")
+	fmt.Println(get_url_from_json())
+}
+
+type JSON_file struct {
+	Slack_url string
+	Username []string
+}
+
+func get_url_from_json() (slack_url string){
+	json_to_decode, err := ioutil.ReadFile("/Users/SchAmToo/Programmings/go_project/actual.json")
+	if err != nil{
+		fmt.Println(err)
+	}
+	//see JSON_file struct
+	var decoded_json JSON_file
+	err = json.Unmarshal(json_to_decode, &decoded_json)
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Println(decoded_json)
+	slack_url = decoded_json.Slack_url
+	return 
 }
 
 func github_rss_feed(username string) {
@@ -30,7 +53,7 @@ func github_rss_feed(username string) {
 }
 
 func slack_bot_post(message string){
-	url_to_use := "https://tech-decks.slack.com/services/hooks/slackbot?token=GiSoJL6O2la9B7r0l3r1o16Y&channel=%23general"
+	url_to_use := "check"
 	fmt.Println(url_to_use)
 	fmt.Println("HEY")
 	var message_bytes = []byte(message)
